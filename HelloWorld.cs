cfa581c2c7ee45d72cafb2670c46e9af2d82fec9
pelmekov <151698156+pelmekov@users.noreply.github.com>
2023-11-22 11:01:28 +0900

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {  
+        
+         print("gg");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
